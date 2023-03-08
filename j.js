f39bdc91b93ce77fbcde6141a86fafe593c99cfe
allen-123-234 <115543347+allen-123-234@users.noreply.github.com>
2023-03-08 21:28:00 +0800

    Add files via upload

--- j.js
+++ j.js
@@ -1,16 +1,15 @@
-let canvas = document.getElementById('game');
-let context = canvas.getContext('2d');
-
-// 绘制背景
-context.fillStyle = '#eee';
-context.fillRect(0, 0, canvas.width, canvas.height);
-
-// 绘制圆形
-context.fillStyle = '#f00';
-context.beginPath();
-context.arc(100, 100, 50, 0, 2 * Math.PI);
-context.fill();
-canvas.addEventListener('touchstart', (event) => {
-    // 处理触摸事件
-  });
+function submitForm() {
+    const form = document.getElementById('myForm');
+    const name = form.elements['name'].value;
+    const email = form.elements['email'].value;
+    const message = form.elements['message'].value;
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://notify-api.line.me/api/notify');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + '<YOUR_ACCESS_TOKEN>');
+  
+    const data = 'message=' + encodeURIComponent('姓名: ' + name + '\n' + '電子郵件: ' + email + '\n' + '訊息: ' + message);
+    xhr.send(data);
+  }
   
